823e04942673bb4054ebeb72d1dac2b56041eef7
fnamlah <faisalmalnamlah@gmail.com>
2023-05-22 22:02:07 +0300

    first commit

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -2,12 +2,12 @@
  * Change the namespace to the namespace on Pinecone you'd like to store your embeddings.
  */
 
-if (!process.env.PINECONE_INDEX_NAME) {
-  throw new Error('Missing Pinecone index name in .env file');
-}
+// if (!process.env.PINECONE_INDEX_NAME) {
+//   throw new Error('Missing Pinecone index name in .env file');
+// }
+// short index name
+const PINECONE_INDEX_NAME = 'vision-2030'
 
-const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
-
-const PINECONE_NAME_SPACE = 'pdf-test'; //namespace is optional for your vectors
+const PINECONE_NAME_SPACE = 'saudi vision 2030'; //namespace is optional for your vectors
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
